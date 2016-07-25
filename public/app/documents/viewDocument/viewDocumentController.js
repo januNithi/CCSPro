6fd249083acdf65aa95565a9fcf5be688514f112
januNithi <csejananim@gmail.com>
2016-07-25 17:00:06 +0530

    Documents path changes

--- public/app/documents/viewDocument/viewDocumentController.js
+++ public/app/documents/viewDocument/viewDocumentController.js
@@ -26,7 +26,7 @@
         $scope.isExist=false;                     //check for document is exist/not
         $scope.isFileExist=false;
         //$scope.isExist=true;
-
+$scope.documentPath = '';
 
         // //Function for getting the parameter from url//
         getParameterByName = function (params) {
@@ -158,7 +158,8 @@
                 $scope.thisFile = UPLOAD_DOC_PATH + $scope.doc.id + '.' + filename.split('.').pop();
                 console.log($scope.doc.docFile );
                 console.log($scope.thisFile );
-                
+                $scope.documentPath = "http://docs.google.com/gview?url=http://ec2-54-213-156-43.us-west-2.compute.amazonaws." +
+                    "com:3000/"+$scope.thisFile + "&embedded=true";
 
             if ($scope.doc.docType === 5) {
                 viewDocumentServices.getArchieve($scope.doc.id)
