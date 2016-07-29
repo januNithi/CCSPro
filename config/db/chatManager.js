c58da7789feba007d4ffd106995de1ed123d1cf9
ppyoosuf <pp.yoosuf@gmail.com>
2016-07-29 14:35:06 +0530

    check the chat

--- config/db/chatManager.js
+++ config/db/chatManager.js
@@ -40,7 +40,7 @@ function getChatinfo() {
     console.log("haaaa");
     var deferred = q.defer();
     var getChatinfo = "select q.id,q.select_userid,q.select_userid as select_userid2,q.msg,q.Dates as date,q.image as image,q.profile_pic as profilePic,q.file,q.my_userid,q.my_userid as my_userid2,personaldata.fname as name,personaldata.mname,personaldata.profile_pic as profilePic,personaldata.lname from tb_sendmsg as q INNER JOIN personaldata ON personaldata.userid=q.my_userid where personaldata.status='active' order by q.Dates DESC";
-    console.log("select * from tb_sendmsg");
+
     con.query(getChatinfo, function (err,result) {
         if (err) {
             deferred.reject(err);
@@ -57,7 +57,7 @@ function getReadinfo(id) {
     console.log("haaaa");
     var deferred = q.defer();
     var getReadinfo = "select q.id,q.my_userid,count(q.meg_read) as readCount from tb_sendmsg q where meg_read = 0 and Q.select_userid='"+id+"' group by q.my_userid" ;
-    console.log("select * from tb_sendmsg");
+
     con.query(getReadinfo, function (err,result) {
         if (err) {
             deferred.reject(err);
@@ -75,7 +75,7 @@ function msgreadInfo(data) {
     console.log("haaaa");
     var deferred = q.defer();
     var msgReadInfo = "UPDATE tb_sendmsg SET meg_read=1 WHERE select_userid='"+data.my_userid+"' and my_userid='"+data.selectUserid+"'";
-    console.log("select * from tb_sendmsg");
+  
     con.query(msgReadInfo, function (err,results) {
         if (err) {
             console.log(err);
