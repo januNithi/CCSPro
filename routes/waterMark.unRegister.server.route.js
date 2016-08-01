6b6be2f8be5ca658d7e1c0c560d16f77498d6c0f
ppyoosuf <pp.yoosuf@gmail.com>
2016-08-01 11:43:06 +0530

    water mark updation

--- routes/waterMark.unRegister.server.route.js
+++ routes/waterMark.unRegister.server.route.js
@@ -1,6 +1,6 @@
-// module.exports=function(app){
-//
-//     // var con=require('../controllers/waterMark.unRegister.server.controller.js');
-//
-//     // app.get('/fileDoc',con.getAllDoc);
-// };
+module.exports=function(app){
+
+    var con=require('../controllers/waterMark.unRegister.server.controller.js');
+
+    app.get('/fileDoc',con.getAllDoc);
+};
