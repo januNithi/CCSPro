34d8f192dd3ddb007a94ef267adc11eab1bc760e
ppyoosuf <pp.yoosuf@gmail.com>
2016-08-02 10:21:00 +0530

    watermark updation

--- lib.js
+++ lib.js
@@ -1,13 +1,13 @@
 var edge = require('edge');
 var path  = require('path');
 
-module.exports = edge.func({
-    source: './office.cs',
-    references: [ 
-        __dirname+'\\config\\pdfConvert\\Microsoft.Office.Interop.Word.dll',
-        __dirname+'\\config\\pdfConvert\\Microsoft.Office.Interop.Excel.dll',
-        __dirname+'\\config\\pdfConvert\\Microsoft.Office.Interop.PowerPoint.dll',
-        __dirname+'\\config\\pdfConvert\\Office.dll',
-        __dirname+'\\config\\pdfConvert\\Microsoft.Vbe.Interop.dll'
-    ],
-});
+// module.exports = edge.func({
+//     source: './office.cs',
+//     references: [
+//         __dirname+'\\config\\pdfConvert\\Microsoft.Office.Interop.Word.dll',
+//         __dirname+'\\config\\pdfConvert\\Microsoft.Office.Interop.Excel.dll',
+//         __dirname+'\\config\\pdfConvert\\Microsoft.Office.Interop.PowerPoint.dll',
+//         __dirname+'\\config\\pdfConvert\\Office.dll',
+//         __dirname+'\\config\\pdfConvert\\Microsoft.Vbe.Interop.dll'
+//     ],
+// });
