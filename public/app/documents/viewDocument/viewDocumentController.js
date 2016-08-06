a388f9128e102bfc5173711088a2e60d53a4160f
ppyoosuf <pp.yoosuf@gmail.com>
2016-08-06 09:51:06 +0530

    change the domain name

--- public/app/documents/viewDocument/viewDocumentController.js
+++ public/app/documents/viewDocument/viewDocumentController.js
@@ -158,7 +158,7 @@ $scope.documentPath = '';
                 $scope.thisFile = UPLOAD_DOC_PATH + $scope.doc.id + '.' + filename.split('.').pop();
                 console.log($scope.doc.docFile );
                 console.log($scope.thisFile );
-                $scope.documentPath = "http://docs.google.com/gview?url=http://ec2-54-186-170-48.us-west-2.compute.amazonaws.com:3000/"+$scope.thisFile + "&embedded=true";
+                $scope.documentPath = "http://docs.google.com/gview?url=http://ec2-54-244-4-126.us-west-2.compute.amazonaws.com:3000/"+$scope.thisFile + "&embedded=true";
 
             if ($scope.doc.docType === 5) {
                 viewDocumentServices.getArchieve($scope.doc.id)
