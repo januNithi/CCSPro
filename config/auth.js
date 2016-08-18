cde78fcce6159d48ecc2196227c782c92b51f333
ppyoosuf <pp.yoosuf@gmail.com>
2016-08-18 16:09:50 +0530

    updation in project module

--- config/auth.js
+++ config/auth.js
@@ -3,11 +3,11 @@ module.exports = {
   facebookAuth: {
     clientID: '261606604222014',
     clientSecret: '7aacc09e77caa57d193c58cfbc272209',
-    callbackURL: 'http://ec2-54-191-209-93.us-west-2.compute.amazonaws.com:3000/auth/facebook/callback'
+    callbackURL: 'http://ec2-54-186-80-166.us-west-2.compute.amazonaws.com:3000/auth/facebook/callback'
   },
   googleAuth: {
     clientID: '825487253382-gj8k49pqvmk10ggdop4kevv7jnaci0n9.apps.googleusercontent.com',
     clientSecret: '1Dws4wswdRg4XR0axhlI37kl',
-    callbackURL: 'http://ec2-54-191-209-93.us-west-2.compute.amazonaws.com:3000/auth/google/callback'
+    callbackURL: 'http://ec2-54-186-80-166.us-west-2.compute.amazonaws.com:3000/auth/google/callback'
   }
 };
