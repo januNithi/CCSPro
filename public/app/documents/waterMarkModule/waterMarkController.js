dfb9ed9caa4ec6aef685f52b1db0a63853fa29f4
ppyoosuf <pp.yoosuf@gmail.com>
2016-08-25 10:41:10 +0530

    updation in project module

--- public/app/documents/waterMarkModule/waterMarkController.js
+++ public/app/documents/waterMarkModule/waterMarkController.js
@@ -97,7 +97,6 @@
 
 
         $scope.showFile = function(id,file) {
-alert(file);
             var thisFile;
             if(file===undefined || file===null) return;
             if (file.split('.').pop() === "doc") {
